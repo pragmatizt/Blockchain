acdd1b349ec320cb1ee55d7411c1f7101c478d30
pragmatizt <pragmatizt@users.noreply.github.com>
2020-04-08 23:12:46 -0400

    After Hours - Module 3

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,7 +13,13 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while not self.valid_proof(block_string, proof):
+        proof += 1
+    return proof
+
+    # This was a copy from blockchain.py
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +33,9 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f"{block_string}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:4] == "0000"
 
 
 if __name__ == '__main__':
@@ -46,6 +54,7 @@ if __name__ == '__main__':
     # Run forever until interrupted
     while True:
         r = requests.get(url=node + "/last_block")
+        print("R", r)
         # Handle non-json response
         try:
             data = r.json()
@@ -56,13 +65,15 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
-
+        print("Mining begins")
+        new_proof = proof_of_work(r)
+        print("Mining complete")
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
+        print(data['message'])
 
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
